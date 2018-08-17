c80246344966ce94d8bebb230446470400c2a144
Preeti Pandey <33650546+Preeti-1010@users.noreply.github.com>
2018-08-17 06:47:10 -1200

    Update p32.php

--- p32.php
+++ p32.php
@@ -1,11 +1,20 @@
 <?php
 	
 	$n = 371;
-	$sum = 0;
+	$len =strlen($n);	
+	$cube = 0;
 	$num = str_split($n); 
-	for($i=0 ;$i<=2; $i++){
-	$cube = $num[$i]*$num[$i]*$num[$i]."<br>";
+	for($i=0 ;$i<=$len; $i++){
+	$cube = $cube + $num[$i]*$num[$i]*$num[$i]."<br>";	
+	//echo $cube;
 	}
+	
+	if($cube == $n){
+		echo "The no is Armstrong :".$cube;
 
+	}
+	else{
+		echo "NOt an Armstrong";
+	}	
 
 ?>
